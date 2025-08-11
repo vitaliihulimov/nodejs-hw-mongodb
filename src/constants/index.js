3954ff7b9d30b3377cea3dc6fac2411dcde91a4a
vitaliigulimov <vitaliygulimov@ukr.net>
2025-08-11 11:45:15 +0300

    add swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,3 +27,5 @@ export const CLOUDINARY = {
   API_KEY: 'CLOUDINARY_API_KEY',
   API_SECRET: 'CLOUDINARY_API_SECRET',
 };
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
